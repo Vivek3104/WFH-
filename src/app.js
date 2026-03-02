0ccc64d80472ec5ec3cf1fe98d8228a06d971181
Vivek3104 <panchalvivek501@gmail.com>
2026-03-02 18:49:52 +0530

    app js updated

--- src/app.js
+++ src/app.js
@@ -11,7 +11,11 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: "GET,POST,PUT,DELETE",
+  allowedHeaders: "Content-Type,Authorization"
+}));
 app.use(express.json());
 app.use(requestLogger);
 
