8a89c1a1508763d3e075894f95e7e0469b4e30db
Vivek3104 <panchalvivek501@gmail.com>
2026-03-02 18:21:06 +0530

    server file updtaed

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import { connectDB } from './config/db.js';
 
 connectDB();
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
